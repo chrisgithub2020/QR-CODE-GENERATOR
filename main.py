260bd01c933009e36e38b1ebfe087dc1dfe6b25a
chrisgithub2020 <agyemanchris0@gmail.com>
2025-10-20 16:15:14 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi.applications import FastAPI, Request
+from fastapi.staticfiles  import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, FileResponse
+from pathlib import Path
+import os
+
+from utils import generate_qr_code
+
+app = FastAPI()
+app.mount("/images", StaticFiles(directory="images"), "image")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html", context={"image_url":"./images/default.png", "download_name":"default.png"})
+
+@app.get("/generate_code/{link}", response_class=HTMLResponse)
+def generate_code(link: str, request: Request):
+
+    file_name = generate_qr_code(link=link)
+    image_path = Path(f"./images/{file_name}.png")
+    if not image_path.is_file():
+        return {"error": "No such image"}
+    
+    return templates.TemplateResponse(request=request, name="replace.html",context={"image_url":f"./images/{file_name}.png", "download_name":file_name})
---
+++ utils.py
@@ -0,0 +1,33 @@
+import qrcode
+from PIL import Image, ImageDraw
+from qrcode.image.styledpil import StyledPilImage
+import uuid
+
+
+def generate_qr_code(link: str):
+    f_name = uuid.uuid4().__str__()
+    qr = qrcode.QRCode(
+        version=5,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+    )
+    qr.add_data(link)
+
+    logo = Image.open("./resources/revna_logo.png").convert("RGBA").resize((95,95), Image.LANCZOS)
+
+
+    qr.make(fit=True)
+    qr_image = qr.make_image(back_color="white", fill_color=(3, 66, 112, 1))
+    
+    ## set position of the logo
+    pos_x = (qr_image.size[0]-logo.width)//2
+    pos_y = (qr_image.size[1]-logo.height)//2
+
+    ## create padding illusion around image
+    draw = ImageDraw.Draw(qr_image)
+    draw.rectangle((pos_x, pos_y, pos_x+100, pos_y+90), fill="white")
+
+    # put image in the code
+    qr_image.paste(logo, (pos_x+5,pos_y), logo)
+    
+    qr_image.save(f"./images/{f_name}.png")
+    return f_name
